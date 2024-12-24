e2fca9dc172fb3c6036ff31e32d654303f2b51b9
guilatrova <hello@guilatrova.dev>
2024-12-24 16:59:11 -0300

    feat: add prologue to evangelho nicodemos

--- copy_ev_nicodemos.py
+++ copy_ev_nicodemos.py
@@ -89,6 +89,23 @@ def _pull_chapters() -> t.Generator[tuple[dict[str, str], int, str], None, None]
 
     yield verses, cur_ch, cur_version
 
+def _fix_grego_latino_prologue(content: Output) -> Output:
+    content["content"]["0"] = "\n".join(
+        [
+            "Eu, ANANIAS, protetor, de hierarquia pretoriana, perito em leis, vim através das divinas Escrituras tomar conhecimento de Nosso Senhor Jesus Cristo e me aproximei dele pela fé, e permiti-me receber o santo batismo; agora sinto-me bem, depois de seguir “a pista das narrações relativas a Nosso Senhor Jesus Cristo, que foram feitas naquela época, e que os judeus deixaram guardadas com Pôncio Pilatos; encontrei-as como estavam, escritas em hebraico, e com o beneplácito divino traduzi-as para o grego, para conhecimento de todos os que invocam o nome de Nosso Senhor Jesus Cristo, durante o reinado de Flávio Teodósio, nosso senhor, no ano 17, e sexto de Flávio Valentino, na nona indicação.",
+            "Todos, pois, quantos leiam e traduzam isto para outros livros, lembrem-se e peçam por mim para que o Senhor seja piedoso para comigo e me perdoe os pecados que cometi contra ele.",
+            "Paz aos leitores, aos ouvintes e aos seus servidores. Amém.",
+            "No ano décimo quinto do governo de Tibério César, imperador dos romanos; no ano décimo nono do governo de Herodes, rei da Galiléia; no oitavo dia das calendas de abril, correspondente ao dia 25 de março; durante o consulado de Rufo e Rubelião; no quarto ano da olimpíada 202; sendo, nessa época, José Caifás o sumo sacerdote dos judeus. Tudo o que Nicodemus narrou com base no tormento da cruz e da paixão do Senhor, transmitiu-o aos príncipes dos sacerdotes e aos demais judeus depois de havê-lo redigido ele mesmo em hebraico.",
+        ]
+    )
+
+    content["titles"] = {
+        "0": "Prólogo",
+        "1": "Narrações Sobre Nosso Senhor Jesus Cristo Compostas no Tempo de Pôncio Pilatos",
+    }
+
+    return content
+
 
 def main() -> None:
     for chapter_content, ch, version in _pull_chapters():
@@ -99,6 +116,9 @@ def main() -> None:
             chapter=ch,
             content=chapter_content
         )
+
+        if version == "grega-e-latina" and ch == 1:
+            new_content = _fix_grego_latino_prologue(new_content)
 
         output_file.parent.mkdir(parents=True, exist_ok=True)
         with open(output_file, "w") as f:
