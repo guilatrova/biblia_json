9918ff4d13b720da723f8e08295d278a21f9a9a4
guilatrova <hello@guilatrova.dev>
2024-12-27 18:31:47 -0300

    feat: split ev nicodemos

--- copy_ev_nicodemos.py
+++ copy_ev_nicodemos.py
@@ -7,7 +7,8 @@ import typing as t
 import json
 
 
-OUTPUT_DIR = Path("./json/apocrifos/pt-br/evangelho-nicodemos/")
+NICODEMOS_OUTPUT_DIR = Path("./json/apocrifos/pt-br/evangelho-nicodemos/")
+DESCIDA_OUTPUT_DIR = Path("./json/apocrifos/pt-br/descida-cristo-inferno/")
 
 class OutputMeta(t.TypedDict):
     title: str
@@ -20,11 +21,12 @@ class Output(t.TypedDict):
     titles: t.NotRequired[dict[str, str]]
 
 
-VERSIONS = ['grego-e-latim', 'grego', 'latim']
+VERSION_T = t.Literal["evangelho-nicodemos", "grego", "latim"]
+VERSIONS: list[VERSION_T] = ['evangelho-nicodemos', 'grego', 'latim']
 
-def _get_version(title: str) -> str:
+def _get_version(title: str) -> VERSION_T:
     if title == "(Versão Grega e Latina)":
-        return "grego-e-latim"
+        return "evangelho-nicodemos"
     if title == "(Versão Grega)":
         return "grego"
     if title == "(Versão Latina)":
@@ -33,16 +35,21 @@ def _get_version(title: str) -> str:
 
 GET_BOOK = "https://verdadeperdida.wordpress.com/2013/03/03/evangelho-de-nicodemus-descida-de-jesus-ao-inferno/"
 
-META: t.Final = OutputMeta(
+NICODEMOS_META: t.Final = OutputMeta(
     title="Evangelho de Nicodemos",
     abbrev="evangelho-nicodemos",
+)
+
+DESCIDA_CRISTO_META: t.Final = OutputMeta(
+    title="Descida de Cristo ao Inferno",
+    abbrev="descida-cristo-inferno",
 )
 
 def compact_json(raw) -> str:
     return json.dumps(raw, separators=(',', ':')).replace("\n", "")
 
 
-def _pull_chapters() -> t.Generator[tuple[dict[str, str], int, str], None, None]:
+def _pull_chapters() -> t.Generator[tuple[dict[str, str], int, VERSION_T], None, None]:
     resp = requests.get(GET_BOOK)
     resp.raise_for_status()
     raw_html = resp.text
@@ -81,7 +88,8 @@ def _pull_chapters() -> t.Generator[tuple[dict[str, str], int, str], None, None]
             verse_num, *verse_content = text.split(" ", maxsplit=1)
 
             if not verse_num.isdigit():
-                verse_num = 1
+                cur_verse_num = max(list([int(k) for k in verses.keys()])) if verses else 0
+                verse_num = cur_verse_num + 1
                 verse_content = text
 
             verses[verse_num] = verse_content
@@ -89,7 +97,7 @@ def _pull_chapters() -> t.Generator[tuple[dict[str, str], int, str], None, None]
 
     yield verses, cur_ch, cur_version
 
-def _fix_grego_latino_prologue(content: Output) -> Output:
+def _fix_ev_nicodemos_prologue(content: Output) -> Output:
     content["content"]["0"] = "\n".join(
         [
             "Eu, ANANIAS, protetor, de hierarquia pretoriana, perito em leis, vim através das divinas Escrituras tomar conhecimento de Nosso Senhor Jesus Cristo e me aproximei dele pela fé, e permiti-me receber o santo batismo; agora sinto-me bem, depois de seguir “a pista das narrações relativas a Nosso Senhor Jesus Cristo, que foram feitas naquela época, e que os judeus deixaram guardadas com Pôncio Pilatos; encontrei-as como estavam, escritas em hebraico, e com o beneplácito divino traduzi-as para o grego, para conhecimento de todos os que invocam o nome de Nosso Senhor Jesus Cristo, durante o reinado de Flávio Teodósio, nosso senhor, no ano 17, e sexto de Flávio Valentino, na nona indicação.",
@@ -109,16 +117,25 @@ def _fix_grego_latino_prologue(content: Output) -> Output:
 
 def main() -> None:
     for chapter_content, ch, version in _pull_chapters():
-        output_file = OUTPUT_DIR / version / f"{ch}.json"
+        if version == "evangelho-nicodemos":
+            meta = NICODEMOS_META
+            output_dir = NICODEMOS_OUTPUT_DIR
+            version_dir = "unknown"
+        else:
+            meta = DESCIDA_CRISTO_META
+            output_dir = DESCIDA_OUTPUT_DIR
+            version_dir = version
+
+        output_file = output_dir / version_dir / f"{ch}.json"
 
         new_content = Output(
-            meta=META,
+            meta=meta,
             chapter=ch,
             content=chapter_content
         )
 
-        if version == "grego-e-latim" and ch == 1:
-            new_content = _fix_grego_latino_prologue(new_content)
+        if version == "evangelho-nicodemos" and ch == 1:
+            new_content = _fix_ev_nicodemos_prologue(new_content)
 
         output_file.parent.mkdir(parents=True, exist_ok=True)
         with open(output_file, "w") as f:
